f29d51d3470d8a9cfc9074e39e1dd0e1699047af
77385393 <38573750+77385393@users.noreply.github.com>
2018-06-01 14:01:13 -0500

    Add files via upload

---
+++ editarperfil_vista.php
@@ -0,0 +1,16 @@
+<?php include 'plantillas/header.php'; ?>
+	</div>
+	<section class="main">
+		<div class="wrapp">
+			<?php include 'plantillas/nav.php'; ?>
+				<article>
+					<div class="mensaje">
+						<h2>PERFIL</h2>
+					</div>
+						<p>..
+						</p><br />						
+				</article>
+	</section>
+<?php include 'plantillas/footer.php'; ?>
+</body>
+</html>
